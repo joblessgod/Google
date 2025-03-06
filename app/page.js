bac3b21de9b01c8794a407437a33685559c9f812
JobLessGod <iamjoblessgod@gmail.com>
2025-03-06 17:49:46 +0545

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,9 +3,9 @@ export default function Home() {
 
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       CodenPen Backend Data
-    <img id="preview" src="https://komarev.com/ghpvc/?username=joblessgod&color=yellow">
+      <img id="preview" src="https://komarev.com/ghpvc/?username=joblessgod&color=yellow" />
     </div>
   );
 }
