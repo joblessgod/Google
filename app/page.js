b68a1cc409ace1c63019754f3ccab75d47e4e89b
JobLessGod <sanchitgamernp@gmail.com>
2025-05-27 23:57:32 +0545

    Chnages

--- app/page.js
+++ app/page.js
@@ -2,10 +2,8 @@
 export default function Home() {
 
 
-  return (
-    <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      CodenPen Backend Data
-      <img id="preview" src="https://komarev.com/ghpvc/?username=joblessgod&color=yellow" />
-    </div>
-  );
-}
+  return(
+    <>
+    <main>Google is Awesome</main>
+    </>
+  )}
